054ad8cb144f48766b9e12abd448e7c06851eae3
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-04-15 08:54:47 +0900

    first commit

--- DoOrDone/DoOrDoneApp.swift
+++ DoOrDone/DoOrDoneApp.swift
@@ -4,14 +4,29 @@
 //
 //  Created by 鶴見駿 on 2024/04/13.
 //
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct DoOrDoneApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
