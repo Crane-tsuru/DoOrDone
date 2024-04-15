f3ff887c244f3ca29cc4039c3ad2f0c90ffb0a78
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-04-15 10:03:45 +0900

    make construct

--- DoOrDone/Models/CoinTossData.swift
+++ DoOrDone/Models/CoinTossData.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
-class CoinTossData: ObservableObject {
+let db = Firestore.firestore()
+
+class CoinTossData: ObservableObject, Identifiable {
+    let id = UUID()
     var date = Date()
     @Published var prediction = true
     @Published var result = ""
+    
+    public func translateDataToDB() -> DBData {
+        let DBdata = DBData(id: id, date: date, prediction: prediction, result: result)
+        return DBdata
+    }
+}
+
+struct DBData: Codable, Identifiable {
+    var id: UUID
+    var date: Date
+    var prediction: Bool
+    var result: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case date
+        case prediction
+        case result
+    }
 }
 
 var judgeMember = ["表", "裏"]
@@ -18,13 +42,19 @@ var resultWord = ["アタリ!", "ハズレ"]
 
 extension CoinTossData {
     
-//    func judege(result: String) -> Bool {
-//        if prediction == result {
-//            return true
-//        } else {
-//            return false
-//        }
-//    }
+    func save(coinTossData: CoinTossData) async {
+        // Add a new document in collection
+        do {
+            try await db.collection("user").document(coinTossData.id.uuidString).setData([
+                "id": coinTossData.id,
+                "date": coinTossData.date,
+                "result": coinTossData.result
+          ])
+          print("Document successfully written!")
+        } catch {
+          print("Error writing document: \(error)")
+        }
+    }
 }
 
 
